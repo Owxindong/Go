8626cc426455ccbbae90b47bfa78b525d60438f3
Owxindong <31104303+Owxindong@users.noreply.github.com>
2023-05-31 01:52:34 +0000

    456

---
+++ ademo/go.mod
@@ -0,0 +1,3 @@
+module ademo
+
+go 1.20
--- zinxdemo/main.go
+++ zinxdemo/main.go
@@ -3,15 +3,22 @@ package main
 import (
 	"fmt"
 	"os"
+	"time"
 )
 
 func main() {
-	dir, err := os.Getwd()
-	if err != nil {
-		fmt.Printf("os.GetWd()执行异常%v", err)
-		return
+	go func() {
+		dir, err := os.Getwd()
+		if err != nil {
+			fmt.Printf("os.GetWd()执行异常%v", err)
+			return
+		}
+		fmt.Println(dir)
+	}()
+
+	select {
+	case <-time.After(time.Second * 20):
+		break
 	}
-	fmt.Println(dir)
-	select {}
 
 }
